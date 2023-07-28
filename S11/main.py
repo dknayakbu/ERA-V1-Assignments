5cebbe11d90d08c9ea3abdf67e4071d3fcbc06f0
dknayakbu <dknayakbu@gmail.com>
2023-07-29 02:49:26 +0530

    more changes

--- S11/main.py
+++ S11/main.py
@@ -87,7 +87,78 @@ test_loader = torch.utils.data.DataLoader(test_set, **dataloader_args)
 # Define the loss function, optimizer and scheduler
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE, weight_decay = WEIGHT_DECAY)
-lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr = SCH_MAX_LR, 
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr = SCH_MAX_LR, 
                                                     steps_per_epoch = len(train_loader), epochs = 30, 
                                                     pct_start = 5/30, div_factor = 100, three_phase = False,
                                                     final_div_factor = 100, anneal_strategy = 'linear')
+
+def train(model, device, train_loader, optimizer, epoch):
+  train_losses = []
+  train_acc = []
+
+  model.train()
+  pbar = tqdm(train_loader)
+  correct = 0
+  processed = 0
+  for batch_idx, (data, target) in enumerate(pbar):
+    # get samples
+    data, target = data.to(device), target.to(device)
+
+    # Init
+    optimizer.zero_grad()
+    # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes.
+    # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+
+    # Predict
+    y_pred = model(data)
+
+    # Calculate loss
+    loss = F.nll_loss(y_pred, target)
+    train_losses = (loss)
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+
+    # Update pbar-tqdm
+
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+    train_acc = (100*correct/processed)
+
+    return train_losses, train_acc
+
+
+def test(model, device, test_loader):
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses = (test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    test_acc = (100. * correct / len(test_loader.dataset))
+
+    return test_losses, test_acc
+
+EPOCHS = 30
+for epoch in range(EPOCHS):
+    print("EPOCH:", epoch)
+    train(model, device, train_loader, optimizer, epoch)
+    scheduler.step()
+    test(model, device, test_loader)
