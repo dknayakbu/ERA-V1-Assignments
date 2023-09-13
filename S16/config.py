e9a1c1ae64753bf9ccaa7629e7da61a74e4eb76d
Deepak <deepak.nayak@braneenterprises.com>
2023-09-13 18:33:57 +0530

    minor changes

--- S16/config.py
+++ S16/config.py
@@ -19,7 +19,7 @@ def get_config():
         "lang_tgt": "fr",
         "model_folder": "weights",
         "model_basename": "tmodel_",
-        "preload": True,
+        "preload": False,
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel"
     }
